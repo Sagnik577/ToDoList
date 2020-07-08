3038814bc8c462a16fc2ce2cf78366864a2f3943
Sagnik577 <61948767+Sagnik577@users.noreply.github.com>
2020-07-08 09:54:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect("mongodb://localhost:27017/db_name",{useNewUrlParser:true, useUnifiedTopology:true});
 
 
 const itemschema= new mongoose.Schema({
